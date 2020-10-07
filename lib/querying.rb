e196ddca12d11525c599ec5f49e00aea331667ff
Ariel Ibarra <arielibarra39@gmail.com>
2020-10-07 05:55:29 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,8 +12,10 @@ end
 
 def select_value_and_count_of_most_prolific_species
   "SELECT species, COUNT(name) 
-  FROM characters 
-  GROUP BY species"
+FROM characters 
+GROUP BY species 
+ORDER BY species DESC
+LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
