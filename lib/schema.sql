c2e474a5b3cfed3632ede2a39e7c73fb2192419e
Ariel Ibarra <arielibarra39@gmail.com>
2020-10-07 04:39:33 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,1 +1,26 @@
-CREATE
+CREATE TABLE series (
+id INTEGER PRIMARY KEY, 
+title TEXT, 
+author_id,
+sub-genre_id);
+
+CREATE TABLE sub-genres (
+id INTEGER PRIMARY KEY,
+name TEXT);
+
+CREATE TABLE authors (
+id INTEGER PRIMARY KEY,
+name = TEXT);
+
+CREATE TABLE books (
+id INTEGER PRIMARY KEY, 
+title TEXT,
+year INTEGER,
+series_id INTEGER);
+
+CREATE TABLE characters (
+id INTEGER PRIMARY KEY,
+name TEXT,
+motto TEXT,
+species TEXT
+author_id INTEGER);
